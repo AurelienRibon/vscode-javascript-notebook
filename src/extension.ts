3dead4676e2d05393c76dab5a9c7986708d95748
Aurelien Ribon <aurelien.ribon@gmail.com>
2020-11-18 21:05:33 +0100

    Draft notebook integration in extension.js

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,33 @@
 import * as vscode from 'vscode';
+import { Notebook } from './lib/notebook';
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand('javascript-notebook.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World from Javascript Notebook!');
+  const disposable = vscode.commands.registerCommand('javascript-notebook.runNotebook', async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    const text = editor.document.getText();
+    const notebook = new Notebook();
+    notebook.refresh(text);
+
+    await editor.edit((eb) => {
+      for (let i = notebook.blocks.length - 1; i >= 0; --i) {
+        const block = notebook.blocks[i];
+
+        if (block.dirty) {
+          const [startLine, endLine] = block.resultRange;
+          const range = new vscode.Range(startLine, 0, endLine, 0);
+
+          const linesDiff = startLine - notebook.linesCount + 1;
+          const prefix = linesDiff > 0 ? '\n'.repeat(linesDiff) : '';
+          const suffix = '\n';
+
+          eb.replace(range, prefix + block.result + suffix);
+        }
+      }
+    });
   });
 
   context.subscriptions.push(disposable);
